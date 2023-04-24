5b3a7d29f490f785d6327edb38f51db729e93263
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-24 03:47:36 -0700

    Main Header File

--- sort.h
+++ sort.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,6 +19,9 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
