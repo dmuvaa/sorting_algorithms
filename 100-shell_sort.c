5a0ae44eb42451c61a39b2aba66fee83a754abec
Dennis Muvaa <dmuvaa70@gmail.com>
2023-04-25 14:24:51 -0700

    function that sorts an array of integers in ascending order using the Shell sort algorithm, using the Knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -14,19 +14,21 @@ void shell_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	/* Knuth sequence for gap */
-	for (gap = 1; gap < size; gap = gap * 3 + 1)
-		;
+	/* Determine the starting gap value */
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-	for (; gap >= 1; gap /= 3)
+	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
 			temp = array[i];
+
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 				array[j] = array[j - gap];
+
 			array[j] = temp;
 		}
-		print_array(array, size);
+		gap = (gap - 1) / 3;
 	}
 }
